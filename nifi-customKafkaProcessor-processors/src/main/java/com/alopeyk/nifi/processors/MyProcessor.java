3396968dca1e24f622eefaef1f25b40ee8c36559
hessam-shm <hessam@ymail.com>
2020-11-15 11:10:24 +0330

    project structure recovered and file recreated manually, with the exception of main processor class

---
+++ nifi-customKafkaProcessor-processors/src/main/java/com/alopeyk/nifi/processors/MyProcessor.java
@@ -0,0 +1,155 @@
+package com.alopeyk.nifi.processors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.annotation.behavior.ReadsAttribute;
+import org.apache.nifi.annotation.behavior.ReadsAttributes;
+import org.apache.nifi.annotation.behavior.WritesAttribute;
+import org.apache.nifi.annotation.behavior.WritesAttributes;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.flowfile.attributes.CoreAttributes;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+@Tags({"customKafkaProcessor"})
+@CapabilityDescription("Provide a description")
+@SeeAlso({})
+@ReadsAttributes({@ReadsAttribute(attribute = "", description = "")})
+@WritesAttributes({@WritesAttribute(attribute = "", description = "")})
+public class MyProcessor extends AbstractProcessor {
+
+    public static final Relationship REL_SUCCESS = new Relationship.Builder()
+            .name("REL_SUCCESS")
+            .description("A Flowfile is routed to this relationship when everything goes well here")
+            .build();
+
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+            .name("REL_FAILURE")
+            .description("A Flowfile is routed to this relationship it can not be parsed or a problem happens")
+            .build();
+
+    private List<PropertyDescriptor> descriptors;
+
+    private Set<Relationship> relationships;
+
+    @Override
+    protected void init(final ProcessorInitializationContext context) {
+        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+
+        this.descriptors = Collections.unmodifiableList(descriptors);
+
+        final Set<Relationship> relationships = new HashSet<Relationship>();
+        relationships.add(REL_SUCCESS);
+        relationships.add(REL_FAILURE);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return descriptors;
+    }
+
+    @OnScheduled
+    public void onScheduled(final ProcessContext context) {
+
+    }
+
+    @Override
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+        FlowFile flowFile = session.get();
+        if (flowFile == null) {
+            return;
+        }
+
+        try {
+            flowFile = session.write(flowFile, (rawIn, rawOut) -> {
+                try (final InputStream in = new BufferedInputStream(rawIn); final OutputStream out =
+                        new BufferedOutputStream(rawOut)) {
+
+                    // Convert input stream flow file into a byte array
+                    byte[] input = toByteArray(in);
+
+                    // Extract fields out of input
+                    Map<String, String> extracted = extractFields(input);
+
+                    // Write json bytes into output
+                    out.write(objectMapper.writeValueAsBytes(extracted));
+                }
+            });
+        } catch (ProcessException pe) {
+            getLogger().error("Failed to deserialize {}", new Object[]{flowFile, pe});
+            session.transfer(flowFile, REL_FAILURE);
+            return;
+        }
+
+        flowFile = session.putAttribute(flowFile, CoreAttributes.MIME_TYPE.key(), "application/json");
+        session.transfer(flowFile, REL_SUCCESS);
+    }
+
+    private Map<String, String> extractFields(final byte[] input) {
+        // extract fields out of the byte array inout
+        return new HashMap<>();
+    }
+
+
+    private byte[] toByteArray(final InputStream in) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+
+        while ((len = in.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+
+        return os.toByteArray();
+    }
+
+    private static KafkaConsumer<GenericRecord, GenericData.Record> createConsumer(){
+        KafkaConsumer<GenericRecord, GenericData.Record> consumer = new KafkaConsumer<>(configConsumerProperties());
+        consumer.subscribe(Collections.singletonList("mysql.alopeyk.orders"));
+        return consumer;
+    }
+
+    private static Properties configConsumerProperties(){
+
+        Properties consumerProperties = new Properties();
+
+        consumerProperties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,"http://172.16.2.220:8081");
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://172.16.2.231:9092," +
+                "http://172.16.2.232:9092,http://172.16.2.220:9092,http://172.16.2.221:9092," +
+                "http://172.16.2.205:9092,http://172.16.2.219:9092");
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "nifiKafkaConsumerTEST");
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return consumerProperties;
+
+    }
+
+}
